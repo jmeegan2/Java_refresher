b808402b89b874b3498fe001394614ba181b2841
jmeegan2 <jmeegan8@gmail.com>
2022-09-01 14:46:38 -0400

    Java basics

---
+++ src/LearnJava.java
@@ -0,0 +1,25 @@
+public class LearnJava {
+    public static void main(String[] args) {
+
+//        //Primitive data types
+//        int a = 5; //stores value of 5 into a
+//        char b = 'i';
+//        long c = 400;
+//        double d = 3.2;
+//
+//        //A string is an object
+//        String name = "Susan";
+//
+//        //the period allows u to manipulate variables
+//        System.out.println(name.toUpperCase());
+//        System.out.println(name.toLowerCase());
+
+        //this part is for the method
+        addExclamationPoint("hot dogs");
+    }
+        //Adding a method
+        public static void addExclamationPoint(String s) {
+            System.out.println(s + "!");
+        }
+    }
+
